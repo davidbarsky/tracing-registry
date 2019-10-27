d1e0cff4e243a7a8730879c2bf6f78e0af214ff7
David Barsky <dbarsky@amazon.com>
2019-10-27 15:25:46 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,144 @@ use std::cell::Cell;
 use std::collections::HashMap;
 use std::convert::TryInto;
 use std::fmt;
+use std::io;
 use std::sync::{Arc, Mutex};
 
+use std::marker::PhantomData;
 use tracing::field::{Field, Visit};
 use tracing::span::Id;
-use tracing::{info, span, Event, Level, Metadata};
+use tracing::{error, info, span, Event, Level, Metadata};
 use tracing_core::{Interest, Subscriber};
 use tracing_subscriber::{layer::Context, Layer};
 
-#[derive(Debug, Default)]
-pub struct StdoutLayer<S: Subscriber> {
-    inner: S,
+/// A type that can create [`io::Write`] instances.
+///
+/// `MakeWriter` is used by [`FmtSubscriber`] to print formatted text representations of
+/// [`Event`]s.
+///
+/// This trait is already implemented for function pointers and immutably-borrowing closures that
+/// return an instance of [`io::Write`], such as [`io::stdout`] and [`io::stderr`].
+///
+/// [`io::Write`]: https://doc.rust-lang.org/std/io/trait.Write.html
+/// [`FmtSubscriber`]: ../struct.Subscriber.html
+/// [`Event`]: https://docs.rs/tracing-core/0.1.5/tracing_core/event/struct.Event.html
+/// [`io::stdout`]: https://doc.rust-lang.org/std/io/fn.stdout.html
+/// [`io::stderr`]: https://doc.rust-lang.org/std/io/fn.stderr.html
+pub trait MakeWriter {
+    /// The concrete [`io::Write`] implementation returned by [`make_writer`].
+    ///
+    /// [`io::Write`]: https://doc.rust-lang.org/std/io/trait.Write.html
+    /// [`make_writer`]: #tymethod.make_writer
+    type Writer: io::Write;
+
+    /// Returns an instance of [`Writer`].
+    ///
+    /// # Implementer notes
+    ///
+    /// [`FmtSubscriber`] will call this method each time an event is recorded. Ensure any state
+    /// that must be saved across writes is not lost when the [`Writer`] instance is dropped. If
+    /// creating a [`io::Write`] instance is expensive, be sure to cache it when implementing
+    /// [`MakeWriter`] to improve performance.
+    ///
+    /// [`Writer`]: #associatedtype.Writer
+    /// [`FmtSubscriber`]: ../struct.Subscriber.html
+    /// [`io::Write`]: https://doc.rust-lang.org/std/io/trait.Write.html
+    /// [`MakeWriter`]: trait.MakeWriter.html
+    fn make_writer(&self) -> Self::Writer;
 }
 
-#[derive(Debug, Default)]
-pub struct StderrLayer {}
+impl<F, W> MakeWriter for F
+where
+    F: Fn() -> W,
+    W: io::Write,
+{
+    type Writer = W;
 
-impl<'a, S: Subscriber + LookupSpan<'a>> Layer<S> for StdoutLayer<S> {
-    fn on_close(&self, id: Id, ctx: Context<S>) {
-        let span = self.inner.span(&id);
-        dbg!(span);
+    fn make_writer(&self) -> Self::Writer {
+        (self)()
     }
 }
 
-impl<S: Subscriber> Layer<S> for StderrLayer {}
+pub struct ConsoleLayer<S: Subscriber, W>
+where
+    W: MakeWriter + Send + Sync + 'static,
+{
+    is_interested: Box<dyn Fn(&Event<'_>) -> bool + Send + Sync + 'static>,
+    make_writer: W,
+    inner: PhantomData<S>,
+}
+
+pub struct ConsoleLayerBuilder<S, W = fn() -> io::Stdout>
+where
+    S: Subscriber,
+    W: MakeWriter + Send + Sync + 'static,
+{
+    is_interested: Box<dyn Fn(&Event<'_>) -> bool + Send + Sync + 'static>,
+    make_writer: W,
+    inner: PhantomData<S>,
+}
+
+impl<S, W> ConsoleLayer<S, W>
+where
+    S: Subscriber,
+    W: MakeWriter + Send + Sync + 'static,
+{
+    fn builder() -> ConsoleLayerBuilder<S> {
+        ConsoleLayerBuilder::default()
+    }
+}
+
+impl<S, W> ConsoleLayerBuilder<S, W>
+where
+    S: Subscriber,
+    W: MakeWriter + Send + Sync + 'static,
+{
+    fn with_interest<F>(&mut self, f: F) -> &mut Self
+    where
+        F: Fn(&Event<'_>) -> bool + Send + Sync + 'static,
+    {
+        self.is_interested = Box::new(f);
+        self
+    }
+
+    fn with_writer(&mut self, w: W) -> &mut Self {
+        self.make_writer = w;
+        self
+    }
+
+    fn build(self) -> ConsoleLayer<S, W> {
+        ConsoleLayer {
+            is_interested: self.is_interested,
+            make_writer: self.make_writer,
+            inner: self.inner,
+        }
+    }
+}
+
+impl<S> Default for ConsoleLayerBuilder<S>
+where
+    S: Subscriber,
+{
+    fn default() -> Self {
+        Self {
+            is_interested: Box::new(|_| true),
+            inner: PhantomData,
+            make_writer: io::stdout,
+        }
+    }
+}
+
+impl<S, W> Layer<S> for ConsoleLayer<S, W>
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+    W: MakeWriter + Send + Sync + 'static,
+{
+    fn on_close(&self, id: Id, ctx: Context<S>) {}
+
+    fn on_event(&self, event: &Event, ctx: Context<S>) {
+        if (self.is_interested)(event) {}
+    }
+}
 
 pub trait LookupSpan<'a> {
     type Span: SpanData<'a> + fmt::Debug;
@@ -79,7 +193,7 @@ impl<'a> SpanData<'a> for Guard<'a, BigSpan> {
 }
 
 #[derive(Debug)]
-struct BigSpan {
+pub struct BigSpan {
     metadata: &'static Metadata<'static>,
     values: Mutex<HashMap<&'static str, Value>>,
     events: Mutex<Vec<BigEvent>>,
@@ -185,8 +299,7 @@ impl Subscriber for Registry {
             None => {
                 if event.is_contextual() {
                     let id = CURRENT_SPAN.with(|s| s.get());
-                    let id = id.expect("Contextual span ID not found");
-                    Some(span::Id::from_u64(id))
+                    id.map(span::Id::from_u64)
                 } else {
                     None
                 }
@@ -213,7 +326,6 @@ impl Subscriber for Registry {
     #[inline]
     fn try_close(&self, id: span::Id) -> bool {
         let span = self.take(id);
-        // dbg!(span);
         true
     }
 }
@@ -227,12 +339,23 @@ impl<'a> LookupSpan<'a> for Registry {
 }
 
 fn main() {
-    let subscriber = Registry::default();
+    // let stderr = ConsoleLayer::builder()
+    //     .with_interest(|event| event.metadata().level() == &Level::ERROR)
+    //     .with_writer(io::stdout)
+    //     .build();
+
+    let stdout = ConsoleLayer::builder()
+        .with_interest(|event| event.metadata().level() == &Level::INFO)
+        .with_writer(io::stdout)
+        .build();
+
+    let subscriber = stdout.with_subscriber(Registry::default());
     tracing::subscriber::set_global_default(subscriber).expect("Could not set global default");
 
     let span = span!(Level::INFO, "my_loop");
     let _entered = span.enter();
-    for i in 0..10 {
+    for i in 0..2 {
+        error!("Closing!");
         span!(Level::INFO, "iteration").in_scope(|| info!(iteration = i, "In a span!"));
     }
 }
